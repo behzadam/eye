5b677c35c5993926d0a7bdd939b4173e3cc89ef3
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-03-21 19:43:34 +0330

    chore: enable minify

--- packages/sdk/tsup.config.ts
+++ packages/sdk/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  format: ["cjs", "esm"],
   dts: true,
   clean: true,
-  sourcemap: true,
+  minify: true,
 });
